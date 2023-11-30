1f4310715d7521dea98b94b86ab270301552ec7c
Daniel Correa <yo@danielgara.com>
2023-11-30 10:21:09 -0500

    imp: improve layout and architecture

--- multipages_bootstrap/App.py
+++ multipages_bootstrap/App.py
@@ -3,6 +3,7 @@ from dash import Dash, html, dcc
 from views.layouts.AppLayout import AppLayout
 from views.pages.HomePage import HomePage
 from views.pages.ArchivePage import ArchivePage
+from views.pages.AnalyticsPage import AnalyticsPage
 
 class App:
     def __init__(self):
@@ -13,9 +14,11 @@ class App:
     def register_pages(self):
         self.home_page = HomePage()
         self.archive_page = ArchivePage()
+        self.analytics_page = AnalyticsPage()
 
         dash.register_page(self.home_page.name, path=self.home_page.path, layout=self.home_page.layout)
         dash.register_page(self.archive_page.name, path=self.archive_page.path, layout=self.archive_page.layout)
+        dash.register_page(self.analytics_page.name, path=self.analytics_page.path, layout=self.analytics_page.layout)
 
     def set_layout(self):
         self.app.layout = AppLayout.define_layout()
--- multipages_bootstrap/views/layouts/AppLayout.py
+++ multipages_bootstrap/views/layouts/AppLayout.py
@@ -8,5 +8,6 @@ class AppLayout:
             html.H1('Multi-page app with Dash Pages'),
             html.Div(dcc.Link('Home', href=dash.page_registry['Home']['path'])),
             html.Div(dcc.Link('Archive', href=dash.page_registry['Archive']['path'])),
+            html.Div(dcc.Link('Analytics', href=dash.page_registry['Analytics']['path'])),
             dash.page_container
         ])
