8c79d52810e21c67774142a161130cd722cae0da
Daniel Correa <yo@danielgara.com>
2023-11-30 09:57:09 -0500

    imp: multipages improved

--- multipages_oop/app.py
+++ multipages_oop/app.py
@@ -1,9 +1,9 @@
 import dash
 from dash import Dash, html, dcc
-from pages.home import HomePage
-from pages.archive import ArchivePage
+from pages.HomePage import HomePage
+from pages.ArchivePage import ArchivePage
 
-class MultiPageApp:
+class App:
     def __init__(self):
         self.app = Dash(__name__, use_pages=True)
         self.register_pages()
@@ -17,17 +17,13 @@ class MultiPageApp:
 
         self.app.layout = html.Div([
             html.H1('Multi-page app with Dash Pages'),
-            html.Div([
-                html.Div(
-                    dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
-                ) for page in dash.page_registry.values()
-            ]),
-            html.Div(dcc.Link('Dashboard', href=dash.page_registry['Archive']['path'])),
+            html.Div(dcc.Link('Home', href=dash.page_registry['Home']['path'])),
+            html.Div(dcc.Link('Archive', href=dash.page_registry['Archive']['path'])),
             dash.page_container
         ])
 
     def run_app(self):
         self.app.run_server(debug=True)
 
-app = MultiPageApp()
+app = App()
 app.run_app()
--- multipages_oop/pages/ArchivePage.py
+++ multipages_oop/pages/ArchivePage.py
@@ -4,7 +4,10 @@ class ArchivePage:
     def __init__(self):
         self.name = 'Archive'
         self.path = '/archive'
-        self.layout = html.Div([
+        self.layout = self.set_layout()
+
+    def set_layout(self):
+        return html.Div([
             html.H1('Archive Page'),
             html.Div('This is the content of the Archive Page.'),
         ])
